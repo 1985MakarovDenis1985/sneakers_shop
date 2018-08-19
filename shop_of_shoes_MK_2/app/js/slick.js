cb29e17bf43971f9e0db978b271f365a159428b5
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-08-19 22:23:31 +0300

    fifth parth

---
+++ shop_of_shoes_MK_2/app/js/slick.js
@@ -0,0 +1,16 @@
+
+$('.slider').slick({
+
+infinite: false,
+dots: true,
+dotsClass: 'maindots',
+slidesToShow: 3,
+slidesToScroll: 1,
+  cssEase: 'linear',
+  arrows: true,
+
+
+prevArrow: '<button class="arrow_left" type="button"><i class="fas fa-chevron-left"></i></button>',
+nextArrow: '<button class="arrow_right" type="button"><i class="fas fa-chevron-right"></i></button>'
+
+});
